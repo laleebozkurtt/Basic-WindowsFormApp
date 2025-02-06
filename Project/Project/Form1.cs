70dce9ee39ed78c40f3fd5cf57d1fa984fa61590
laleebozkurtt <162982040+laleebozkurtt@users.noreply.github.com>
2025-02-06 17:30:46 +0300

    Aplication Completed

---
+++ Project/Project/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            if (username == "lale" && password == "12345")
+            {
+                toolStripStatusLabel1.Text = "Giriş başarılı! Hoş geldiniz.";
+                toolStripStatusLabel1.ForeColor = Color.Red; 
+                Form3 form3 = new Form3();
+                form3.Show();
+                this.Hide(); 
+
+            }
+            else
+            {
+                toolStripStatusLabel1.Text = "Hatalı kullanıcı adı veya şifre!";
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Project/Project/Form6.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Project
+{
+    public partial class Form6 : Form
+    {
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }        
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        string belgeyolu, belgeadi;
+
+        private void btnOlustur_Click(object sender, EventArgs e)
+        {
+            belgeadi = txtBelgeAdı.Text;
+            StreamWriter sw = File.CreateText(belgeyolu + "\\" + belgeadi + ".txt");
+            toolStripStatusLabel1.Text = "Belgeniz başarıyla oluşturuldu.!";
+
+        }
+
+        private void btnOku_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader oku = new StreamReader(openFileDialog1.FileName);
+                string satir = oku.ReadLine();
+                while (satir != null)
+                {
+                    listBox1.Items.Add(satir);
+                    satir = oku.ReadLine();
+                }
+            }
+        }
+
+        private void dosyaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnKonumSec_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+               belgeyolu = folderBrowserDialog1.SelectedPath;
+               label1.Text = belgeyolu;
+
+            }
+
+        }
+        
+    }
+}
